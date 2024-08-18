5554df9b63d865966ba1ce9ac80c7e782e3ddf8c
Ntwali Axel <axel@Ntwalis-MacBook-Air.local>
2024-08-18 23:59:26 +0200

    Adjusted admin_view

--- django-models/relationship_app/admin_view.py
+++ django-models/relationship_app/admin_view.py
@@ -5,6 +5,6 @@ from django.contrib.auth.decorators import user_passes_test
 def is_admin(user):
     return user.is_authenticated and user.userprofile.role == 'Admin'
 
-@user_passes_test(is_admin)
+@user_passes_test(is_admin, login_url='/login/')
 def admin_view(request):
     return render(request, 'admin_view.html')
