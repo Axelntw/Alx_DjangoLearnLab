ba6a188ae21d95c6c521f601d703a215e4ecdde4
Ntwali Axel <axel@Ntwalis-MacBook-Air.local>
2024-08-19 00:01:06 +0200

    Adjusted admin_view

--- django-models/relationship_app/admin_view.py
+++ django-models/relationship_app/admin_view.py
@@ -1,9 +1,12 @@
 # admin_view.py
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
+from django.core.exceptions import PermissionDenied
 
 def is_admin(user):
-    return user.is_authenticated and user.userprofile.role == 'Admin'
+    if user.is_authenticated and user.userprofile.role == 'Admin':
+        return True
+    raise PermissionDenied
 
 @user_passes_test(is_admin, login_url='/login/')
 def admin_view(request):
