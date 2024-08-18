2cdf0751438ba6cbdc91aa5fe3229bfa2cb75368
Ntwali Axel <axel@Ntwalis-MacBook-Air.local>
2024-08-18 23:31:05 +0200

    Correction

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import list_books
+import views
+
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', views.CustomLoginView.as_view(), name='login'),
+    path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -3,6 +3,11 @@ from django.views.generic import ListView
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth import authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView, LogoutView
 
 def list_books(request):
     books = Book.objects.all()
@@ -17,3 +22,23 @@ class LibraryDetailView(ListView):
         context = super().get_context_data(**kwargs)
         context['books'] = self.object.books.all()
         return context
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')  # Redirect to your home page
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+
+class CustomLogoutView(LogoutView):
+    template_name = 'logout.html'
