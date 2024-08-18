237002489f2877e3e1c0dbe5af54a4a2643e1cc8
Ntwali Axel <axel@Ntwalis-MacBook-Air.local>
2024-08-18 21:37:36 +0200

    Added template for listing books and library details

--- django-models/django_models/urls.py
+++ django-models/django_models/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from . import views
+from relationship_app import views
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
