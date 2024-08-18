a8e74c8492ec7a9a32251161dcf8cc5981df3bb5
Ntwali Axel <axel@Ntwalis-MacBook-Air.local>
2024-08-18 23:42:12 +0200

    Adjusted urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
     path('register/', views.register, name='register'),
+    path('', views.home, name='home'),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -4,10 +4,11 @@ from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login
-from django.contrib.auth import authenticate
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.decorators import login_required
 
 def list_books(request):
     books = Book.objects.all()
@@ -23,22 +24,32 @@ class LibraryDetailView(ListView):
         context['books'] = self.object.books.all()
         return context
 
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return render(request, 'logout.html')
+
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)
+            user = form.save()
             login(request, user)
-            return redirect('home')  # Redirect to your home page
+            return redirect('home')
     else:
         form = UserCreationForm()
     return render(request, 'register.html', {'form': form})
 
-class LoginView(LoginView):
-    template_name = 'login.html'
-
-class LogoutView(LogoutView):
-    template_name = 'logout.html'
+@login_required
+def home(request):
+    return render(request, 'home.html')
