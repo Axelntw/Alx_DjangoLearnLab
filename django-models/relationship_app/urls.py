9d2bcab80435e55774ba2ccdf81cf227e94d9488
Ntwali Axel <axel@Ntwalis-MacBook-Air.local>
2024-08-22 20:38:44 +0200

    Added imports

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
-from . import views
+from . import admin_view, librarian_view, member_view
+from .views import list_books, login_view, logout_view, register, home, LibraryDetailView  
 
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('register/', views.register, name='register'),
-    path('', views.home, name='home'),
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('register/', register, name='register'),
+    path('', home, name='home'),
     path('admin-view/', admin_view.admin_view, name='admin_view'),
     path('librarian-view/', librarian_view.librarian_view, name='librarian_view'),
     path('member-view/', member_view.member_view, name='member_view'),
