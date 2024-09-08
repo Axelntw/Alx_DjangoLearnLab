629ca22ffbfa51f812fe9abecf59260c4b5afb42
Ntwali Axel <axel@Ntwalis-MacBook-Air.local>
2024-09-08 21:40:31 +0200

    Initial commit

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,9 +1,14 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
-from rest_framework.permissions import IsAuthenticated
 
 class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
+
+class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated]
