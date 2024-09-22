6c11c0b5d047279f5e88b0c8a61db6dd032b17b4
Axelntw <axel@Ntwalis-MacBook-Air.local>
2024-09-22 14:50:21 +0200

    Initial commit

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
-from .forms import ExtendedUserCreationForm, PostForm
+from .forms import ExtendedUserCreationForm, CommentForm
 from django.contrib import messages
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
-from .models import Post
+from .models import Post, Comment
 
 def register(request):
     if request.method == 'POST':
@@ -29,6 +29,40 @@ def profile(request):
         messages.success(request, 'Profile updated successfully.')
         return redirect('profile')
     return render(request, 'registration/profile.html')
+
+@login_required
+def add_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.author = request.user
+            comment.save()
+            return redirect('post-detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/add_comment.html', {'form': form})
+
+@login_required
+def edit_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id, author=request.user)
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            form.save()
+            return redirect('post-detail', pk=comment.post.pk)
+    else:
+        form = CommentForm(instance=comment)
+    return render(request, 'blog/edit_comment.html', {'form': form})
+
+@login_required
+def delete_comment(request, comment_id):
+    comment = get_object_or_404(Comment, id=comment_id, author=request.user)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post-detail', pk=post_pk)
 
 class PostListView(ListView):
     model = Post
